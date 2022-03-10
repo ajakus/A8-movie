eb3af7ac07344fcaf3e2c4f1cfecc814a0ff9a1b
ajakus <60129215+ajakus@users.noreply.github.com>
2022-03-09 23:34:18 -0600

    Add files via upload

---
+++ Services/JsonService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+
+namespace ApplicationTemplate.Services;
+
+/// <summary>
+///     This concrete service and method only exists an example.
+///     It can either be copied and modified, or deleted.
+/// </summary>
+public class JsonService : IJsonService
+{
+    
+
+    public abstract class Movie
+    {
+        public static void Read(string json)
+        {
+            Product p = JsonConvert.DeserializeObject<Product>(json);
+
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p.Expiry);
+            Console.WriteLine(p.Sizes);
+
+        }
+
+        public void Write()
+        {
+            Product product = new Product();
+            product.Name = "Apple";
+            product.Expiry = new DateTime(2008, 12, 28);
+            product.Sizes = new string[] { "Small" };
+
+            // Install-Package Newtonsoft.Json
+            string json = JsonConvert.SerializeObject(product);
+
+            Console.WriteLine(json);
+
+            return json;
+        }
+        internal class Product()
+        {
+            public string Name { get; set; }
+            public DateTime Expiry { get; set; }
+
+            public string[] Sizes { get; set; }
+        }
+    }
+    
+}
---
+++ Services/Video.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationTemplate.Services
+{
+    public class Video : Media
+    {
+        public override string Display()
+        {
+            string file = "C:/Users/jakus/Downloads/ApplicationTemplate-master/ApplicationTemplate-master/videos.csv";
+
+
+            StreamReader sr = new StreamReader(file);
+
+            string line = sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                line = sr.ReadLine();
+                var column = line.Split(',');
+                var sv = string.Join(',', column);
+                Console.WriteLine(sv);
+
+            }
+            sr.Close();
+            throw new NotImplementedException();
+        }
+        public static string Write()
+        {
+            Product product = new Product();
+            product.Name = "Apple";
+            product.Expiry = new DateTime(2008, 12, 28);
+            product.Sizes = new string[] { "Small" };
+            
+            Install-Package Newtonsoft.Json
+            string json = JsonConvert.SerializeObject(product);
+
+            Console.WriteLine(json);
+
+            return json;
+        }
+
+        public static void Read(string json)
+        {
+            Product p = JsonConvert.DeserializeObject<Product>(json);
+
+            Console.WriteLine(p.Name);
+            Console.WriteLine(p.Expiry);
+            Console.WriteLine(p.Sizes);
+
+        }
+    }
+}
